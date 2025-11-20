e6dbe4f49b601ccc37b5a3b752d5e694cfd91062
7005215-wmci <7005215@spsd.sk.ca>
2025-11-20 09:35:04 -0600

    Started on the early basic movement system.

--- sketch.js
+++ sketch.js
@@ -5,12 +5,34 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x = 5;
+let y = 5;
+let speed = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 50);
+  background("white"); 
+  if (keyIsDown(87) === true){
+    y = y - speed;
+  }
+  if (keyIsDown(83) === true){
+    y = y + speed;
+  }
+  if (keyIsDown(65) === true){
+    x = x - speed;
+  }
+  if (keyIsDown(68) === true){
+    x = x + speed;
+  }
+  updatePosition();
 }
+
+function updatePosition(){
+  fill("black");
+  square(x, y, 70);
+}
+
+
