b64db3c81b0ddb6d04d1595d0c70ebd9b4abc9b1
7005215-wmci <7005215@spsd.sk.ca>
2025-12-01 09:37:40 -0600

    Border and Barrier Storage Complete

--- sketch.js
+++ sketch.js
@@ -5,19 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x = 50;
-let y = 50;
+let x = 90;
+let y = 90;
 let size = 70;
 let oldX = x;
 let oldY = y;
 let speed = 5;
-let barrier_X_positions = [200];
-let barrier_Y_positions = [200];
-let barrier_widths = [400];
-let barrier_heights = [80];
+let barrier_X_positions = [200, 400];
+let barrier_Y_positions = [200, 240];
+let barrier_widths = [400, 200];
+let barrier_heights = [80, 120];
+let map = 1;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
+  background('#8a8787ff');
 }
 
 function draw() {
@@ -46,16 +48,39 @@ function draw() {
 }
 
 function updatePosition(){
-  fill("black");
+  background('#8a8787ff');
+  fill("blue");
   square(x, y, 70);
-  rect(barrier_X_positions[0], barrier_Y_positions[0], barrier_widths[0], barrier_heights[0]);
-}
-
-function checkOnBarrier(){
-  if (x  > barrier_X_positions[0] - size && x < barrier_X_positions[0] + barrier_widths[0] && y > barrier_Y_positions[0] - size  && y < barrier_Y_positions[0] + barrier_heights[0]){
-    x = oldX;
-    y = oldY;
+  fill("black");
+  if (map === 1){
+    rect(barrier_X_positions[0], barrier_Y_positions[0], barrier_widths[0], barrier_heights[0]);
+    rect(barrier_X_positions[1], barrier_Y_positions[1], barrier_widths[1], barrier_heights[1]);
   }
 }
 
-
+function checkOnBarrier(){
+  if (x > 800 - size|| x < 0 || y > 800 - size|| y < 0){
+    x = oldX;
+    y = oldY;
+  }
+  if (map === 1){
+    if (x  > barrier_X_positions[0] - size && x < barrier_X_positions[0] + barrier_widths[0] && y > barrier_Y_positions[0] - size  && y < barrier_Y_positions[0] + barrier_heights[0]){
+      x = oldX;
+      y = oldY;
+    }
+    if (x  > barrier_X_positions[1] - size && x < barrier_X_positions[1] + barrier_widths[1] && y > barrier_Y_positions[1] - size  && y < barrier_Y_positions[1] + barrier_heights[1]){
+      x = oldX;
+      y = oldY;
+    }
+  }
+  if (map === 2){
+    if (x  > barrier_X_positions[2] - size && x < barrier_X_positions[2] + barrier_widths[2] && y > barrier_Y_positions[2] - size  && y < barrier_Y_positions[2] + barrier_heights[2]){
+      x = oldX;
+      y = oldY;
+    }
+    if (x  > barrier_X_positions[1] - size && x < barrier_X_positions[1] + barrier_widths[1] && y > barrier_Y_positions[1] - size  && y < barrier_Y_positions[1] + barrier_heights[1]){
+      x = oldX;
+      y = oldY;
+    }
+  }
+}
